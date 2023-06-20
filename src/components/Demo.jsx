8cbe2019d0416c699f72d32caa90b94d1b052282
Sidney Pang <sidneypang@hotmail.co.uk>
2023-06-20 16:18:15 -0400

    added browser link history

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -5,15 +5,45 @@ import { copy, linkIcon, loader, tick } from "../assets";
 import { useLazyGetSummaryQuery } from "../services/article";
 
 const Demo = () => {
+  // set current article info
   const [article, setArticle] = useState({
     url: "",
     summary: "",
   });
 
+  // history store of all articles
+  const [allArticles, setAllArticles] = useState([]);
+
+  // hook from redux where getsummary is the function to fetch from api on submit
   const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
 
+  useEffect(() => {
+    const articlesFromLocalStorage = JSON.parse(
+      localStorage.getItem("articles")
+    );
+
+    if (articlesFromLocalStorage) {
+      setAllArticles(articlesFromLocalStorage);
+    }
+  }, []);
+
   const handleSubmit = async (e) => {
-    alert("Submitted");
+    e.preventDefault();
+
+    const { data } = await getSummary({
+      articleUrl: article.url,
+    });
+
+    if (data?.summary) {
+      const newArticle = { ...article, summary: data.summary };
+
+      const updatedAllArticles = [newArticle, ...allArticles];
+
+      setArticle(newArticle);
+      setAllArticles(updatedAllArticles);
+
+      localStorage.setItem("articles", JSON.stringify(updatedAllArticles));
+    }
   };
 
   return (
@@ -22,9 +52,7 @@ const Demo = () => {
       <div className="flex flex-col w-full gap-2">
         <form
           className="relative flex justify-center items-center"
-          onSubmit={() => {
-            handleSubmit;
-          }}
+          onSubmit={handleSubmit}
         >
           <img
             src={linkIcon}
@@ -52,6 +80,28 @@ const Demo = () => {
         </form>
 
         {/* Browser URL History */}
+        <div className="flex flex-col gap-1 max-h-60 overflow-y-auto">
+          {allArticles.map((item, index) => (
+            <div
+              key={`link-${index}`}
+              onClick={() => {
+                setArticle(item);
+              }}
+              className="link_card"
+            >
+              <div className="copy_btn">
+                <img
+                  src={copy}
+                  alt="copy_icon"
+                  className="w-[40%] h-[40%] object-contain"
+                />
+              </div>
+              <p className="flex-1 font-satoshi text-blue-700 font-medium text-sm truncate">
+                {item.url}
+              </p>
+            </div>
+          ))}
+        </div>
       </div>
 
       {/* Display Results */}
